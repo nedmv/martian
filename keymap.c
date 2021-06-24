7239616517b5f21ee8d7ae127410114acdece8e8
nedmv <nedmvl@rambler.ru>
2021-06-24 21:16:11 +0300

    fix: remove unused layer toggles

--- keymap.c
+++ keymap.c
@@ -63,8 +63,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_moonlander(
     KC_ESCAPE,      KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_EQUAL,                                       KC_TRANSPARENT, KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,       
     OSL(3),         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_LBRACKET,                                    KC_DOT,         KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_TRANSPARENT, 
-    TG(1),          KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_RBRACKET,                                                                    KC_COMMA,       KC_H,           KC_J,           KC_K,           KC_L,           LT(3,KC_SCOLON),LGUI_T(KC_QUOTE),
-    KC_CAPSLOCK,    KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         RCTL_T(KC_SLASH),ST_MACRO_0,     
+    TG(1),          KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_RBRACKET,                                                                    KC_COMMA,       KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON, KC_QUOTE,
+    KC_CAPSLOCK,    KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH, ST_MACRO_0,     
     KC_GRAVE,       TD(DANCE_0),    KC_LGUI,        KC_LALT,        KC_LCTRL,       KC_TRANSPARENT,                                                                                                 TO(1),          TT(2),          TT(3),          TT(4),          KC_BSLASH,      LGUI(KC_L),     
     KC_SPACE,       KC_BSPACE,      KC_DELETE,                      KC_TAB,         KC_ENTER,       KC_LSHIFT
   ),
