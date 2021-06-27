17ec0b69b96569af62dcaf961bbd95b8a84ae42d
nedmv <nedmvl@rambler.ru>
2021-06-27 13:00:08 +0300

    fix: add F11 and F12

--- keymap.c
+++ keymap.c
@@ -100,7 +100,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     XXXXXXX,
     KC_MS_BTN1,   KC_MS_BTN2,    KC_MS_BTN3,
 
-    _______,      KC_F6,         KC_F7,       KC_F8,         KC_F9,          KC_F10,     _______,
+    KC_F11,       KC_F6,         KC_F7,       KC_F8,         KC_F9,          KC_F10,     KC_F12,
     _______,      _______,       KC_HOME,     KC_PGUP,       KC_PGDOWN,      KC_END,     _______, 
     _______,      _______,       KC_LEFT,     KC_UP,         KC_DOWN,        KC_RIGHT,   _______,   
     _______,      LCTL(KC_LEFT), LCTL(KC_UP), LCTL(KC_DOWN), LCTL(KC_RIGHT), _______,              
