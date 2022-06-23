09acce0c90b562ba4d6fd50545535222e2c66f0c
nedmv <nedmvl@rambler.ru>
2022-06-24 01:32:00 +0300

    fix: change compose key, duplicate arrow positions

--- lighting.c
+++ lighting.c
@@ -80,7 +80,7 @@ const uint8_t PROGMEM ledmap[][DRIVER_LED_TOTAL][3] = {
         _________,     COLOR_DBLUE,   COLOR_DBLUE,   COLOR_DBLUE,   COLOR_DBLUE,   COLOR_DBLUE,   _________,
         _________,     COLOR_YELLOW,  COLOR_YELLOW,  COLOR_YELLOW,  COLOR_YELLOW,  _________,     _________,
         _________,     COLOR_ORANGE,  COLOR_ORANGE,  COLOR_ORANGE,  COLOR_ORANGE,  _________,     _________,
-        _________,     _________,     _________,     _________,     _________,     _________,
+        _________,     COLOR_PURPLE,  COLOR_PURPLE,  COLOR_PURPLE,  COLOR_PURPLE,  _________,
         _________,     _________,     _________,     _________,     _________,
         _________,
         _________,     _________,     _________,
